5d3caa17bb0639caa2a51fd1defeb88fa2e7113b
Leandro (cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2026-09-08 11:39:37 -0300

    Use jekyll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,98 +1,96 @@
 (function ($) {
-	// Close menu after click on smaller screens
-	$(window).on("resize", function () {
-		let menu = $("#menu");
-		if ($(window).width() < 846) {
-			$(".main-menu a").on("click", function () {
-				menu.removeClass("open");
-			});
-		}
-	});
+  // Close menu after click on smaller screens
+  $(window).on("resize", function () {
+    let menu = $("#menu");
+    if ($(window).width() < 846) {
+      $(".main-menu a").on("click", function () {
+        menu.removeClass("open");
+      });
+    }
+  });
 
-	function showSection(section, isAnimate) {
-		if (section === "")
-			return;
+  function showSection(section, isAnimate) {
+    if (section === "")
+      return;
 
-		window.location.hash = section;
-		let direction = section.replace(/#/, "");
-		let selector = `[data-section="${direction}"]`;
-		let reqSection = $(selector);
-		let reqSectionPos = reqSection.offset().top;
+    window.location.hash = section;
+    let direction = section.replace(/#/, "");
+    let selector = `[data-section="${direction}"]`;
+    let reqSection = $(selector);
+    let reqSectionPos = reqSection.offset().top;
 
-		if (isAnimate) {
-			$("body, html").animate(
-				{
-					scrollTop: reqSectionPos
-				},
-				800
-			);
-		} else {
-			$("body, html").scrollTop(reqSectionPos);
-		}
-	}
+    if (isAnimate) {
+      $("body, html").animate(
+        {
+          scrollTop: reqSectionPos
+        },
+        800
+      );
+    } else {
+      $("body, html").scrollTop(reqSectionPos);
+    }
+  }
 
-	function checkSection() {
-		$(".section").each(function () {
-			let $this = $(this);
-			let topEdge = $this.offset().top - 80;
-			let bottomEdge = topEdge + $this.height();
-			let wScroll = $(window).scrollTop();
+  function checkSection() {
+    $(".section").each(function () {
+      let $this = $(this);
+      let topEdge = $this.offset().top - 80;
+      let bottomEdge = topEdge + $this.height();
+      let wScroll = $(window).scrollTop();
 
-			if (topEdge < wScroll && bottomEdge > wScroll) {
-				let currentId = $this.data("section");
-				let reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+      if (topEdge < wScroll && bottomEdge > wScroll) {
+        let currentId = $this.data("section");
+        let reqLink = $("a").filter("[href*=\\#" + currentId + "]");
 
-				reqLink
-					.closest("li")
-					.addClass("active")
-					.siblings()
-					.removeClass("active");
+        reqLink
+          .closest("li")
+          .addClass("active")
+          .siblings()
+          .removeClass("active");
 
-			}
-		});
-	}
+      }
+    });
+  }
 
-	$(window).scroll(function () {
-		checkSection();
-	});
+  $(window).scroll(function () {
+    checkSection();
+  });
 
-	$(".responsive-nav").load("/portfolio/main_menu.html", function () {
-		$("#menu-toggle").on("click", function (e) {
-			let menu = $("#menu");
+  $("#menu-toggle").on("click", function (e) {
+    let menu = $("#menu");
 
-			if (menu.hasClass("open")) {
-				menu.removeClass("open");
-			} else {
-				menu.addClass("open");
-			}
-		});
+    if (menu.hasClass("open")) {
+      menu.removeClass("open");
+    } else {
+      menu.addClass("open");
+    }
+  });
 
-		$("#menu-close").on("click", function (e) {
-			$("#menu").removeClass("open");
-		});
+  $("#menu-close").on("click", function (e) {
+    $("#menu").removeClass("open");
+  });
 
-		$(".main-menu").on("click", "a", function (e) {
-			let currPage = window.location.pathname;
+  $(".main-menu").on("click", "a", function (e) {
+    let currPage = window.location.pathname;
 
-			if (currPage === "/portfolio/")
-				currPage = "/portfolio/index.html";
+    if (currPage === "/portfolio/")
+      currPage = "/portfolio/index.html";
 
-			let currHref = $(this).attr("href");
+    let currHref = $(this).attr("href");
 
-			if (currHref.includes(currPage) && currHref.includes("")) {
-				e.preventDefault();
-				let destination = currHref.split('#')[1];
-				showSection(destination, true);
-			}
-		});
+    if (currHref.includes(currPage) && currHref.includes("")) {
+      e.preventDefault();
+      let destination = currHref.split('#')[1];
+      showSection(destination, true);
+    }
+  });
 
-		$(".shortcut").on("click", "", function (e) {
-			e.preventDefault();
+  $(".shortcut").on("click", "", function (e) {
+    e.preventDefault();
 
-			showSection($(this).attr("href"), true);
-		});
+    showSection($(this).attr("href"), true);
+  });
 
-		showSection(window.location.hash, false);
-		checkSection();
-	});
+  showSection(window.location.hash, false);
+  checkSection();
 })(jQuery);
